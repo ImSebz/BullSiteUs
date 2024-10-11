4acb5cfd328d082f73604bc99e42771b584ec888
Sebastián Camargo <juansecapri@hotmail.com>
2024-10-11 15:01:54 -0500

    Componente de multimedia agregado

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,22 @@ import './App.css';
 import { Header } from './components/Header/Header';
 import { Multimedia } from './components/Multimedia/Multimedia';
 import { Slogan } from './components/Slogan/Slogan';
-import { Services } from './components/Services/Services'; 
-import { PossibleInfo } from './components/PossibleInfo/PossibleInfo'; 
-import { Contact } from './components/Contact/Contact'; 
+import { Services } from './components/Services/Services';
+import { PossibleInfo } from './components/PossibleInfo/PossibleInfo';
+import { Contact } from './components/Contact/Contact';
 import { Footer } from './components/Footer/Footer';
- 
+
 function App() {
 
   return (
     <>
-      <Header/>
-      <Slogan/>
-      {/* <Multimedia/> */}
-      <Services/>
-      <PossibleInfo/>
-      <Contact/>
-      <Footer/>
+      <Header />
+      <Slogan />
+      {/* <Multimedia /> */}
+      <Services />
+      <PossibleInfo />
+      <Contact />
+      <Footer />
     </>
   )
 }
